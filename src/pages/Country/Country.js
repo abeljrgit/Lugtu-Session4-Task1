79e174bb017e9f21328cd9d269ba4cde46a96487
ALKHWARIZMI-PC\abelm <abelmanuellugtujr1996@gmail.com>
2023-02-24 11:39:22 +0800

    🚑 fix: Remove Fragment

--- src/pages/Country/Country.js
+++ src/pages/Country/Country.js
@@ -20,57 +20,55 @@ export const Country = () => {
   }, []);
 
   return (
-    <>
-      <section className="center-h section-about-us ">
-        <Container
-          sx={{
-            display: 'flex',
-            justifyContent: 'center',
-          }}
-        >
-          {responseData.length ? (
-            <Card
-              className="card"
-              sx={{
-                display: 'flex',
-                flexDirection: 'column',
-              }}
-            >
-              <div className="card-img">
-                <img
-                  src={responseData[0]?.flags?.png}
-                  alt={`Flag of ${responseData[0]?.name?.common}`}
-                />
-              </div>
-              <Box sx={{ p: 2, mt: 'auto' }}>
-                <Typography variant="h6">
-                  {responseData[0]?.name?.common}
-                </Typography>
+    <section className="center-h section-about-us ">
+      <Container
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+        }}
+      >
+        {responseData.length ? (
+          <Card
+            className="card"
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+            }}
+          >
+            <div className="card-img">
+              <img
+                src={responseData[0]?.flags?.png}
+                alt={`Flag of ${responseData[0]?.name?.common}`}
+              />
+            </div>
+            <Box sx={{ p: 2, mt: 'auto' }}>
+              <Typography variant="h6">
+                {responseData[0]?.name?.common}
+              </Typography>
 
-                <Typography
-                  variant="body1"
-                  sx={{ fontSize: 14 }}
-                >{`Population: ${responseData[0]?.population}`}</Typography>
-                <Typography
-                  variant="body1"
-                  sx={{ fontSize: 14 }}
-                >{`Capital: ${responseData[0]?.capital[0]}`}</Typography>
-                <Typography sx={{ fontSize: 12, color: '#E96479' }}>
-                  <a
-                    className="card-map__link"
-                    href={responseData[0]?.maps?.googleMaps ?? ''}
-                    target="_blank"
-                  >
-                    Google Map
-                  </a>
-                </Typography>
-              </Box>
-            </Card>
-          ) : (
-            <div>No Data.</div>
-          )}
-        </Container>
-      </section>
-    </>
+              <Typography
+                variant="body1"
+                sx={{ fontSize: 14 }}
+              >{`Population: ${responseData[0]?.population}`}</Typography>
+              <Typography
+                variant="body1"
+                sx={{ fontSize: 14 }}
+              >{`Capital: ${responseData[0]?.capital[0]}`}</Typography>
+              <Typography sx={{ fontSize: 12, color: '#E96479' }}>
+                <a
+                  className="card-map__link"
+                  href={responseData[0]?.maps?.googleMaps ?? ''}
+                  target="_blank"
+                >
+                  Google Map
+                </a>
+              </Typography>
+            </Box>
+          </Card>
+        ) : (
+          <div>No Data.</div>
+        )}
+      </Container>
+    </section>
   );
 };
